f4b58a2c7d684dd2d0ae4bb900b12fe064d17329
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2017-04-20 16:52:17 +0200

    veramente infernale

--- src/main/java/org/kj6682/hop/DatabaseFiller.java
+++ src/main/java/org/kj6682/hop/DatabaseFiller.java
@@ -27,7 +27,7 @@ public class DatabaseFiller {
 
         @Override
         public void run(String... args) throws Exception {
-            System.out.println("******  running the SapleDataCLR *******");
+            System.out.println("******  running the AccountRepositoryFiller *******");
             Stream.of("john,lennon", "paul,mccarntey", "george,harrison", "ringo,starr")
                     .map(x -> x.split(","))
                     .forEach(tuple -> accountRepository.save(new Account(tuple[0], tuple[1], true)));
@@ -47,11 +47,12 @@ public class DatabaseFiller {
 
         @Override
         public void run(String... args) throws Exception {
-            System.out.println("******  running the SapleDataCLR *******");
+            System.out.println("******  running the HopRepositoryFiller *******");
             Stream.of("1984,george orwell, book, shelf1",
                     "der prozess,franz kafka, book, shelf1",
                     "amerika,franz kafka, book, shelf1",
-                    "decameron,giovanni boccaccio, book, shelf2"
+                    "decameron,giovanni boccaccio, book, shelf2",
+                    "la divina commedia, dante alighieri, book, shelf2"
             )
                     .map(x -> x.split(","))
                     .forEach(tuple -> hopRepository.save(new Hop(tuple[0], tuple[1], tuple[2], tuple[3])));
